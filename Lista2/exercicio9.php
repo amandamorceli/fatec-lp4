841906bb4a9f0fd3560a6b62151cc0f75a512343
amandamorceli <amandamorceli@gmail.com>
2024-09-02 12:02:17 -0300

    mais alguns exercícios da lista 2

--- Lista2/exercicio9.php
+++ Lista2/exercicio9.php
@@ -3,31 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Resposta 8</title>
+    <title>Exercício 9</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <main class="container">
-      <h2 class="mt-4">Resposta do exercício 8</h2>
-      <?php
-      if($_SERVER["REQUEST_METHOD"]=='POST')
-      {
-          try
-          {
-              $altura = (float)$_POST['altura'] ?? 0;
-              $largura = (float)$_POST['largura'] ?? 0;
-          
-              $retangulo = $altura * $largura;
-              $retangulo =  number_format($retangulo, 2, '.', '');
-
-              echo "<p>Área do retângulo: $retangulo.";
-          } 
-          catch(Exception $e)
-          {
-              "Erro!".$e->getMessage();
-          }
-      } 
-      ?>
+        <h2 class="mt-4">Exercício 9</h2>
+        <form action="exercicio9resp.php" method="POST">
+            <div class="row mt-4">
+                <div class="col-auto">
+                    <label for="raio" class="form-label">Digite o raio do círculo:</label>
+                    <input type="number" name="raio" id="raio" class="form-control">
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+            </div>
+        </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
