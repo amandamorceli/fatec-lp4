f9e9360c3c0921fbf617ba98569f1ba12e2e5c97
amandamorceli <amandamorceli@gmail.com>
2024-10-01 00:19:46 -0300

    finalizando mais alguns exercícios

--- Lista5/exercicio5.2/index.php
+++ Lista5/exercicio5.2/index.php
@@ -34,4 +34,75 @@ declare(strict_types=1);
             </div>
         </div>
     </form>
-    <?php ?>
+    <?php
+declare(strict_types=1);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 5.2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="container mt-3">
+    <h2 class="">Exercício 2</h2>
+    <form action="" method="POST">
+        <?php for ($i = 0; $i <= 4; $i++): ?>
+            <div class="row mt-3">
+                <div class="col-3">
+                    <label for="nomes<?= $i ?>" class="form-label">Informe o nome: </label>
+                    <input type="text" class="form-control" name="nomes[]" id="nomes<?= $i ?>" placeholder="Nome<?= $i +1?>" required>
+                </div>
+                <?php for ($j = 0; $j <= 2; $j++): ?>
+                    <div class="col-2">
+                        <div class="mb-3">
+                            <label for="notas<?= $i ?><?= $j ?>" class="form-label">Informe a nota: </label>
+                            <input type="number" class="form-control" name="notas[<?= $i ?>][]" id="notas<?= $i ?><?= $j ?>" step="0.1" placeholder="Nota<?= $j +1?>" required>
+                        </div>
+                   </div>
+                <?php endfor; ?>
+            </div>
+        <?php endfor; ?>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+
+    <?php
+
+    function calcularMedias(array $nomes, array $notas): array 
+    {
+        $medias = [];
+
+    for ($i = 0; $i < count($nomes); $i++) 
+        {
+            $nome = $nomes[$i];
+            $somaNotas = array_sum($notas[$i]);
+            $media = $somaNotas / 3;  
+            $medias[$nome] = $media;
+        }
+
+        arsort($medias);
+
+        return $medias;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+        $nomes = $_POST['nomes'];
+        $notas = $_POST['notas'];
+
+        $medias = calcularMedias($nomes, $notas);
+
+        echo nl2br("<p>Médias dos Alunos:</p>\n");
+        foreach ($medias as $nome => $media) 
+        {
+            echo "<p>{$nome}: " . number_format($media, 2) . "</p>";
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ Lista5/exercicio5.3/index.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 5.3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="container mt-3">
+    <h2 class="">Exercício 3 </h2>
+    <form action="" method="POST">
+        <?php for ($i = 0; $i <= 4; $i++): ?>
+            <div class="row mt-3">
+                <div class="col-2">
+                    <label for="codigo<?= $i ?>" class="form-label">Código: </label>
+                    <input type="text" class="form-control" name="codigos[]" id="codigo<?= $i ?>" placeholder="Código<?= $i + 1 ?>" required>
+                </div>
+                <div class="col-4">
+                    <label for="nome<?= $i ?>" class="form-label">Nome: </label>
+                    <input type="text" class="form-control" name="nomes[]" id="nome<?= $i ?>" placeholder="Nome<?= $i + 1 ?>" required>
+                </div>
+                <div class="col-2">
+                    <label for="preco<?= $i ?>" class="form-label">Preço: </label>
+                    <input type="number" step="0.01" class="form-control" name="precos[]" id="preco<?= $i ?>" placeholder="Preço<?= $i + 1 ?>" required>
+                </div>
+            </div>
+        <?php endfor; ?>
+        <div class="row mt-3">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+
+    <?php
+
+    function ProcessarProdutos(array $codigos, array $nomes, array $precos): array 
+    {
+        $produtos = [];
+
+        for ($i = 0; $i < count($codigos); $i++) 
+        {
+            $codigo = $codigos[$i];
+            $nome = $nomes[$i];
+            $preco = (float)$precos[$i];
+
+            if ($preco > 100.00) {
+                $preco *= 0.9; 
+            }
+
+            $produtos[$codigo] = [
+                'nome' => $nome,
+                'preco' => $preco
+            ];
+        }
+        return $produtos;
+    }
+
+    function OrdenarProdutosPorNome($produtos): array
+    {
+        uasort($produtos, function($a, $b) 
+        {
+            return strcmp($a['nome'], $b['nome']);
+        });
+
+        return $produtos;
+    }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+        {
+        
+            $codigos = $_POST['codigos'];
+            $nomes = $_POST['nomes'];
+            $precos = $_POST['precos'];
+
+            $produtosProcessados = ProcessarProdutos($codigos, $nomes, $precos);
+            $produtosOrdenados = OrdenarProdutosPorNome($produtos);
+
+
+            echo "<p>Lista de Produtos:</p>";
+        }
+    ?>
+</body>
+</html>
